6cd44acfc58f671bdc2aaf5fbee2d0ff06b6689e
Geraldo Gonzalez <geraldodeveloper09@gmail.com>
2019-10-08 13:58:27 -0400

    Funcionalidades agregadas.
    Se han añadido las funcionalidades para recortar los nombres de los jugadores, equipos, y toda la funcionalidad para la interfaz de rankings,

--- code/js/app.js
+++ code/js/app.js
@@ -1,22 +1,57 @@
 // VARIABLES
 let tablaInfo = document.querySelector('.tabla-standings');
+
+// Variables para controlar el dropdown menu.
 let mqDropdown = window.matchMedia('(max-width: 1024px)');
 let menu = document.querySelector('.menu li:nth-child(2)');
 let submenu = document.querySelector('.menu .submenu');
+
+// Flechas para el player history de la interfaz de standings.
 let desplazar = 0;
+
+// Botones de XBOX y PS4 para la interfaz de rankings.
+let btnRanking = document.querySelector('.btn-ranking');
+
+// Slide para la interfaz de rankings
+let slide = document.querySelector('.slide');
+let tabla = document.querySelectorAll('.tabla-rank');
+
+// Main de rounds.
+let cards = document.querySelectorAll('.border-card .card');
+
 
 // EVENTS LISTENERS
 
 cargarEventListeners();
 
 function cargarEventListeners(){
+    
     if(tablaInfo) {
         tablaInfo.addEventListener('click', mostrarPartidos);
         tablaInfo.addEventListener('click', siguiente);
         tablaInfo.addEventListener('click', anterior);
+        window.addEventListener('load', acortarNombresTabla);
     }
+
+    if(cards) {
+        window.addEventListener('load', acortarNombresCards);
+    }
+
     if(mqDropdown.matches){
-        menu.addEventListener('mouseover', dropdownMenu)
+        menu.addEventListener('mouseover', dropdownMenu);
+    }
+
+    if (btnRanking) {
+        btnRanking.addEventListener('click', tablaRank);
+    }
+
+    if (slide) {
+        slide.addEventListener('click', desplazarFilas);
+    }
+
+    if(tabla) {
+        window.addEventListener('resize', restaurarTabla);
+        window.addEventListener('load', acortarNombresRank);
     }
 }
 
@@ -26,7 +61,7 @@ function cargarEventListeners(){
 /* FUNCIONES. */
 
 // Función que muestra el historial del player.
-function mostrarPartidos(e){
+function mostrarPartidos(e) {
 	if(e.target.classList.contains('oculto')){
         let btnInfo = e.target.parentElement;
         let playerInfo = e.target.parentElement.parentElement.parentElement;
@@ -61,9 +96,9 @@ function mostrarPartidos(e){
 }
 
 
-// Función que permite mover las cards en el slider.
 
-function siguiente(e){
+// Función que permite mover las cards en el slider.
+function siguiente(e) {
     if(e.target.classList.contains('fa-chevron-right')){
         desplazar += 1; 
         let contenedorSlide = e.target.parentElement.parentElement;
@@ -85,7 +120,7 @@ function siguiente(e){
     }   
 }
 
-function anterior(e){
+function anterior(e) {
     if(e.target.classList.contains('fa-chevron-left')){
         desplazar = desplazar - 1; 
         e.target.parentElement.previousSibling.previousSibling.style.display = 'grid';
@@ -98,6 +133,9 @@ function anterior(e){
     }   
 }
 
+
+
+// Función para el dropdown mobile.
 function dropdownMenu(e) {
     e.preventDefault();
     if(e.target.classList.contains('fa-chevron-down')) {
@@ -123,4 +161,141 @@ function dropdownMenu(e) {
         document.querySelector('.fa-chevron-down').style.height = '100%';
         document.querySelector('.fa-chevron-down').style.alignItems = 'center';
     }
+}
+
+
+
+// Función para el menu de ranking.
+function tablaRank(e) {
+    const tablaXbox = document.querySelector('.tabla-rank.xbox');
+    const tablaPs4 = document.querySelector('.tabla-rank.ps4');
+    const rankXbox = document.querySelector('.rank-consola.xbox');
+    const rankPs4 = document.querySelector('.rank-consola.ps4');
+    const filas = document.querySelectorAll('.player-info');
+
+    if(e.target.classList.contains('btn-ps4') ) {        
+        filas.forEach( (fila) => {
+            fila.classList.remove('derecha');
+            fila.classList.remove('izquierda');
+            document.querySelector('.slide .izquierda').classList.remove('active');
+            document.querySelector('.slide .derecha').classList.add('active');
+        });
+
+        e.target.classList.add('active');
+        e.target.parentElement.lastChild.previousSibling.classList.remove('active');
+        
+        rankXbox.classList.remove('active');
+        tablaXbox.classList.remove('active');
+        rankXbox.classList.add('inactive');
+        tablaXbox.classList.add('inactive');
+        tablaPs4.classList.remove('inactive');
+        tablaPs4.classList.add('active');
+        rankPs4.classList.add('active');
+
+    } else if(e.target.classList.contains('btn-xbox') ) {        
+        filas.forEach( (fila) => {
+            fila.classList.remove('derecha');
+            fila.classList.remove('izquierda');
+            document.querySelector('.slide .izquierda').classList.remove('active');
+            document.querySelector('.slide .derecha').classList.add('active');
+        });
+
+        e.target.classList.add('active');
+        e.target.parentElement.firstChild.nextSibling.classList.remove('active');
+        
+        rankPs4.classList.remove('active');
+        tablaPs4.classList.remove('active');
+        rankPs4.classList.add('inactive');
+        tablaPs4.classList.add('inactive');
+        tablaXbox.classList.remove('inactive');
+        tablaXbox.classList.add('active');
+        rankXbox.classList.add('active');
+    }
+}
+
+
+
+// Función para las filas de la tabla
+function desplazarFilas(e) {
+    
+    const filas = document.querySelectorAll('.player-info');
+    if(e.target.classList.contains('izquierda')) {
+        filas.forEach( (fila) => {
+            fila.classList.remove('derecha');
+            fila.classList.add('izquierda');
+            e.target.parentElement.firstChild.nextSibling.classList.remove('active');
+            e.target.classList.add('active');
+        });
+    } else if(e.target.classList.contains('derecha')) {
+        filas.forEach( (fila) => {
+            fila.classList.remove('izquierda');
+            fila.classList.add('derecha');
+            e.target.parentElement.lastChild.previousSibling.classList.remove('active');
+            e.target.classList.add('active');
+        });
+    }
+}
+
+
+
+// Función para que la tabla vuelva sus valores a la normalidad al cambiar el tamaño de la pantalla.
+function restaurarTabla() {
+    let filas = document.querySelectorAll('.player-info');
+    let pantalla = window.matchMedia('(min-width: 680px)');
+    if(pantalla.matches) {
+        filas.forEach( (fila) => {
+            fila.classList.remove('derecha');
+            fila.classList.remove('izquierda');
+            document.querySelector('.slide .izquierda').classList.remove('active');
+            document.querySelector('.slide .derecha').classList.add('active');
+        });
+    }
+}
+
+
+
+// Función para recortar los nombres de los jugadores.
+function acortarNombresRank() {
+
+    const nombres = document.querySelectorAll('.player-info td .player span'); 
+    nombres.forEach( nombre => {
+        if(nombre.childNodes[0].data.length >= 15) {
+            nombre.childNodes[0].data = nombre.childNodes[0].data.substr(0, 12) + '...';
+        }
+    } );
+
+}
+
+function acortarNombresTabla(){
+
+    const nombres = document.querySelectorAll('.player-info td:first-child span.NimbusSanCon-Bol');
+    nombres.forEach( nombre => {
+        nombre.childNodes[0].data = nombre.childNodes[0].data.trim();
+        if(nombre.childNodes[0].data.length >= 15) {
+            nombre.childNodes[0].data = nombre.childNodes[0].data.substr(0, 12) + '...';
+        }
+    } );
+
+}
+
+
+
+function acortarNombresCards() {
+    
+    const nombresCards = document.querySelectorAll('.border-card .card .nation-nick .nick p');
+    const nombresTeam = document.querySelectorAll('.border-card .card .players-info-games .player .team');
+
+    nombresCards.forEach( nombreCard => {
+        nombreCard.childNodes[0].data = nombreCard.childNodes[0].data.trim();
+        if(nombreCard.childNodes[0].data.length >= 15) {
+            nombreCard.childNodes[0].data = nombreCard.childNodes[0].data.substr(0, 12) + '...';
+        }
+    } );
+
+    nombresTeam.forEach( nombreTeam => {
+        nombreTeam.childNodes[0].data = nombreTeam.childNodes[0].data.trim();
+        if(nombreTeam.childNodes[0].data.length >= 15) {
+            nombreTeam.childNodes[0].data = nombreTeam.childNodes[0].data.substr(0, 17) + '...';
+        }
+    } );
 }
